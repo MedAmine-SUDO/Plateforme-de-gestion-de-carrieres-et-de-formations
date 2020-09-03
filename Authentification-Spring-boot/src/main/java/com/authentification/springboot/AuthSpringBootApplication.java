a9b14098ca70ec1a78b7e6027119f9bbb9f85b2e
akrem-zerelli <akrem.zerelli@gmail.com>
2020-09-03 11:38:18 +0100

    adding authentication interfaces and editing authentification controller services

---
+++ Authentification-Spring-boot/src/main/java/com/authentification/springboot/AuthSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.authentification.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringBootApplication.class, args);
+	}
+
+}
---
+++ Authentification-Spring-boot/src/main/java/com/authentification/springboot/controllers/AuthController.java
@@ -0,0 +1,137 @@
+package com.authentification.springboot.controllers;
+
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.authentification.springboot.models.ERole;
+import com.authentification.springboot.models.Role;
+import com.authentification.springboot.models.User;
+import com.authentification.springboot.payload.request.LoginRequest;
+import com.authentification.springboot.payload.request.SignupRequest;
+import com.authentification.springboot.payload.response.JwtResponse;
+import com.authentification.springboot.payload.response.MessageResponse;
+import com.authentification.springboot.repository.RoleRepository;
+import com.authentification.springboot.repository.UserRepository;
+import com.authentification.springboot.security.jwt.JwtUtils;
+import com.authentification.springboot.security.services.UserDetailsImpl;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();		
+		List<String> roles = userDetails.getAuthorities().stream()
+				.map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+
+		return ResponseEntity.ok(new JwtResponse(jwt, 
+												 userDetails.getId(), 
+												 userDetails.getUsername(), 
+												 userDetails.getEmail(), 
+												 roles));
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Username is already taken!"));
+		}
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Email is already in use!"));
+		}
+
+		// Create new user's account
+		User user = new User(signUpRequest.getUsername(), 
+							 signUpRequest.getEmail(),
+							 encoder.encode(signUpRequest.getPassword()));
+
+		Set<String> strRoles = signUpRequest.getRoles();
+		Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+			Role candidatRole = roleRepository.findByName(ERole.ROLE_CANDIDAT)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(candidatRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+				case "admin":
+					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(adminRole);
+
+					break;
+				case "instructor":
+					Role instructorRole = roleRepository.findByName(ERole.ROLE_INSTRUCTOR)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(instructorRole);
+
+					break;
+				case "mentor":
+					Role mentorRole = roleRepository.findByName(ERole.ROLE_MENTOR)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(mentorRole);
+
+					break;
+				default:
+					Role candidatRole = roleRepository.findByName(ERole.ROLE_CANDIDAT)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(candidatRole);
+				}
+			});
+		}
+
+		user.setRoles(roles);
+		userRepository.save(user);
+
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+}

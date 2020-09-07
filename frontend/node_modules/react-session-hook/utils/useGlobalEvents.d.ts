b2bff040096f2a4f61bff05fe2fb822aff949b59
akrem-zerelli <akrem.zerelli@gmail.com>
2020-09-07 15:26:41 +0100

    Delete frontend

--- frontend/node_modules/react-session-hook/utils/useGlobalEvents.d.ts
+++
@@ -1,4 +0,0 @@
-import { UseSession } from "../interfaces";
-declare const _default: <TProfile>({ globalLogin, globalLogout, setSession, removeSession, storage }: UseSession<TProfile>) => void;
-export default _default;
-//# sourceMappingURL=useGlobalEvents.d.ts.map
--- frontend/src/reducers/RootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import auth from "./AuthReducer"
-const RootReducer = combineReducers({
-  auth: auth
-});
-
-export default RootReducer;

eb5a07a75d3da41378e486e206b6a10c2dbdb95c
MedAmine-SUDO <mohamedamine.benafia@esprims.com>
2020-09-22 13:16:17 +0100

    Add Swagger to testProfile

--- learningStyleTest_Service/src/main/java/com/carthageSolution/learningStyleTest/controller/QuestionController.java
+++ learningStyleTest_Service/src/main/java/com/carthageSolution/learningStyleTest/controller/QuestionController.java
@@ -2,10 +2,15 @@ package com.carthageSolution.learningStyleTest.controller;
 
 import com.carthageSolution.learningStyleTest.model.Question;
 import com.carthageSolution.learningStyleTest.service.QuestionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,24 +19,41 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/testProfile/question")
+@Api(value = "TestProfileQuestionAPI", produces = MediaType.APPLICATION_JSON_VALUE, description = "TP Question Resource")
+
 public class QuestionController {
 
     @Autowired
     private QuestionService questionService;
 
     //Get all questions
+    @ApiOperation("Get All Questions")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully get Questions"),
+            @ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+            @ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+            @ApiResponse(code = 404, message = "The resource  not found")})
     @GetMapping("")
     public List<Question> getAllQuestions(){
         return questionService.findAll();
     }
 
     //Get specefic question by its number
+    @ApiOperation("Get Question By QuestionNbr")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully get Question"),
+            @ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+            @ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+            @ApiResponse(code = 404, message = "The resource  not found")})
     @GetMapping("/{nbr}")
     public Optional<Question> getQuestionByNbr(@PathVariable("nbr") Integer nbr){
         return questionService.findByQuestionNbr(nbr);
     }
 
     //Create question
+    @ApiOperation("Create Question")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully create Question"),
+            @ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+            @ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+            @ApiResponse(code = 404, message = "The resource  not found")})
     @PostMapping("/createQuestion")
     public ResponseEntity<?> createQuestion(@RequestBody Question question){
         questionService.createQuestion(question);
@@ -39,12 +61,22 @@ public class QuestionController {
     }
 
     //Delete a question by it's id
+    @ApiOperation("Delete Question By ID")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully delete Question"),
+            @ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+            @ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+            @ApiResponse(code = 404, message = "The resource  not found")})
     @DeleteMapping("/delete/{id}")
     public void deleteQuestion(@PathVariable("id") String id){
         questionService.deleteQuestion(id);
     }
 
     //Delete all questions
+    @ApiOperation("Delete All Questions")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully delete all Questions"),
+            @ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+            @ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+            @ApiResponse(code = 404, message = "The resource  not found")})
     @DeleteMapping("/deleteAll")
     public ResponseEntity<?> deleteAllQuestions(){
         questionService.deleteAll();
@@ -52,6 +84,11 @@ public class QuestionController {
     }
 
     //Update a question
+    @ApiOperation("Update Question")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully update Question"),
+            @ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+            @ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+            @ApiResponse(code = 404, message = "The resource  not found")})
     @PutMapping("/{id_question}")
     public ResponseEntity<Question> updateQuestion(@PathVariable("id_question") String id_question, @RequestBody Question question){
         Optional<Question> newQuestion = questionService.findById(id_question);
--- learningStyleTest_Service/src/main/java/com/carthageSolution/learningStyleTest/controller/ResultController.java
+++ learningStyleTest_Service/src/main/java/com/carthageSolution/learningStyleTest/controller/ResultController.java
@@ -5,7 +5,12 @@ import com.carthageSolution.learningStyleTest.model.Category;
 import com.carthageSolution.learningStyleTest.model.Question;
 import com.carthageSolution.learningStyleTest.model.Test;
 import com.carthageSolution.learningStyleTest.service.TestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +23,19 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/testProfile/result")
+@Api(value = "TestProfileResultAPI", produces = MediaType.APPLICATION_JSON_VALUE, description = "TP Result Resource")
+
 public class ResultController {
 
     @Autowired
     private TestService testService;
 
     //Get your learning style by specifiying test id
+    @ApiOperation("Get Your LearningStyle by Test ID")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully get LearningStyle"),
+            @ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+            @ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+            @ApiResponse(code = 404, message = "The resource  not found")})
     @GetMapping("/style/{id}")
     public String getlearningStyle(@PathVariable("id") String id){
         StringBuilder style= new StringBuilder("Your learning style is ");
@@ -43,6 +55,11 @@ public class ResultController {
     }
 
     //Get test result as a HashMap
+    @ApiOperation("Get Test Result")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully get Result"),
+            @ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+            @ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+            @ApiResponse(code = 404, message = "The resource  not found")})
     @GetMapping("/{id}")
     public HashMap<String, Integer> getResult(@PathVariable("id") String id) {
         HashMap<String, Integer> map = new HashMap<>();

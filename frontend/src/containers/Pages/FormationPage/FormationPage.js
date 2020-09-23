069a28d9cd7be05d178e699f819ac211d158884c
akrem-zerelli <akrem.zerelli@gmail.com>
2020-09-23 12:47:44 +0100

    add formation user inetrface

---
+++ frontend/src/containers/Pages/FormationPage/FormationPage.js
@@ -0,0 +1,41 @@
+import React from "react";
+import classNames from "classnames";
+import { makeStyles } from "@material-ui/core/styles";
+import styles from "../../../assets/jss/material-kit-react/views/formationPage.js";
+import GridContainer from "../../../components/Grid/GridContainer.js";
+import GridItem from "../../../components/Grid/GridItem.js";
+import Footer from "../../../components/Footer/Footer.js";
+
+const useStyles = makeStyles(styles);
+function FormationPage() {
+  const classes = useStyles();
+  return (
+    <div>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+            <GridItem xs={12} sm={12} md={6}>
+                <div className={classes.profile}>
+                  <div className={classes.name}>
+                    <h2>write HERE</h2>
+                  </div>
+                </div>
+              </GridItem>
+              <GridItem xs={12} sm={12} md={6}>
+                <div className={classes.profile}>
+                  <div className={classes.name}>
+                    <h2>write HERE</h2>
+                  </div>
+                </div>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default FormationPage;
